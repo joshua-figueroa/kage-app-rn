cf61b20b3c624a8b57ed858feee9f50d2ce7a457
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2020-12-29 19:44:01 +0800

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppearanceProvider } from "react-native-appearance";
+import { Provider } from "react-redux";
+
+import AppNavigator from "./app/navigations/AppNavigator";
+import theme from "./app/navigations/navigationTheme";
+import store from "./app/redux/store";
+import useNotifications from "./app/hooks/useNotifications";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    useNotifications();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <Provider store={store}>
+            <AppearanceProvider>
+                <NavigationContainer theme={theme}>
+                    <AppNavigator />
+                </NavigationContainer>
+            </AppearanceProvider>
+        </Provider>
+    );
+}
